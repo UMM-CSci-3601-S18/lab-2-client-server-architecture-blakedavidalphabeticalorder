67c45260ac876036588b82fed5548a3b1f186ee7
chong050 <chong050@morris.umn.edu>
2018-02-12 23:17:50 -0600

    In todo.js and todo.html Added button and input for todo limit
    -David

--- src/main/resources/public/javascript/todo.js
+++ src/main/resources/public/javascript/todo.js
@@ -58,6 +58,15 @@ function getAllTodosByStatus() {
     document.getElementById('jsonDump').innerHTML = returned_json;
   });
 }
+
+function getAllTodosByLimit() {
+  console.log("Getting all the todos.");
+
+  var HttpThingy = new HttpClient();
+  HttpThingy.get("/api/todos?limit=" +  document.getElementById("limit").value, function(returned_json){
+    document.getElementById('jsonDump').innerHTML = returned_json;
+  });
+}
 /**
  * Wrapper to make generating http requests easier. Should maybe be moved
  * somewhere else in the future!.
