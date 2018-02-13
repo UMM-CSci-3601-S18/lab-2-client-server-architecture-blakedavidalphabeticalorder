62ed1128856d2d34a1a1ce6d59d76a6937879757
chong050 <chong050@morris.umn.edu>
2018-02-12 21:23:34 -0600

    Modified todo.js and todo.html Added get todos by category input and button
    close #31
    
    -David

--- src/main/resources/public/javascript/todo.js
+++ src/main/resources/public/javascript/todo.js
@@ -40,6 +40,15 @@ function getAllTodosByOwner() {
     document.getElementById('jsonDump').innerHTML = returned_json;
   });
 }
+
+function getAllTodosByCategory() {
+  console.log("Getting all the todos.");
+
+  var HttpThingy = new HttpClient();
+  HttpThingy.get("/api/todos?category=" +  document.getElementById("category").value, function(returned_json){
+    document.getElementById('jsonDump').innerHTML = returned_json;
+  });
+}
 /**
  * Wrapper to make generating http requests easier. Should maybe be moved
  * somewhere else in the future!.
